c28f492eb4a6fb93b779b959775a64c32ba5b852
yazilimci0 <yazilim8118@gmail.com>
2023-09-08 11:36:02 +0300

    Proje Kod
    deneme

---
+++ SinemaSeruveni/SinemaSeruveni/Models/Siniflar/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SinemaSeruveni.Models.Siniflar
+{
+    public class Blog
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string BlogImage { get; set; }
+    }
+}
